e3e59859f3cc3fad817ed1f2d4e7ce39057d458c
Dany Walraven <dwalraven21@gmail.com>
2019-10-18 22:45:58 -0400

    added some routing configuration with react-router-dom

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,18 @@
 import React, {Component, Fragment} from 'react';
-import { Container } from 'semantic-ui-react'
-import EventDashboard from '../../features/event/eventDashboard/EventDashboard'
+
+import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
+
 import NavBar from '../../features/nav/navBar/NavBar'
+
+import HomePage from '../../features/home/HomePage'
+import EventDashboard from '../../features/event/eventDashboard/EventDashboard'
+import EventDetails from '../../features/event/eventDetails/EventDetails'
+import PeopleDashboard from '../../features/users/peopleDashboard/PeopleDashboard'
+import UserDetails from '../../features/users/userDetails/UserDetails'
+import SettingsDashboard from '../../features/users/settings/SettingsDashboard'
+import EventForm from '../../features/event/eventForm/EventForm'
+
 
 class App extends Component {
 	render() {
@@ -9,7 +20,34 @@ class App extends Component {
 		<Fragment>
 			<NavBar />
 			<Container className="main">
-				<EventDashboard />
+				<Route
+					path="/"
+					component={HomePage}
+				/>
+				<Route
+					path="/events"
+					component={EventDashboard}
+				/>
+				<Route
+					path="/events/:id"
+					component={EventDetails}
+				/>
+				<Route
+					path="/people"
+					component={PeopleDashboard}
+				/>
+				<Route
+					path="/profile/:id"
+					component={UserDetails}
+				/>
+				<Route
+					path="/settings"
+					component={SettingsDashboard}
+				/>
+				<Route
+					path="/create"
+					component={EventForm}
+				/>
 			</Container>
 		</Fragment>
 	    );
